42c74511a0faeb87e7203567706ae2cbd2c2671d
Lucas Jerome Lovelace <lucasjeromelovelace@Lucass-Air.attlocal.net>
2023-10-27 21:44:04 -0500

    updated script and html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,73 +1,103 @@
 var questions = [
-    {
-      title: 'Commonly used data types DO NOT include:',
-      choices: ['strings', 'booleans', 'alerts', 'numbers'],
-      answer: 'alerts',
-    },
-    {
-      title: 'The condition in an if / else statement is enclosed within ____.',
-      choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-      answer: 'parentheses',
-    },
-    {
-      title: 'Arrays in JavaScript can be used to store ____.',
-      choices: [
-        'numbers and strings',
-        'other arrays',
-        'booleans',
-        'all of the above',
-      ],
-      answer: 'all of the above',
-    },
-    {
-        title:
-          'String values must be enclosed within ____ when being assigned to variables.',
-        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-        answer: 'quotes',
-      },
-      {
-        title:
-          'A very useful tool used during development and debugging for printing content to the debugger is:',
-        choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
-        answer: 'console.log',
-      },
-    ];
-var time=60;
-var TimerEl=document.getElementById("Timer");
-var startEl=document.getElementById("start");
-var startbtnEl=document.getElementById("start-btn");
-var quizEl=document.getElementById("Quiz-play");
-var questionEl=document.getElementById("question");
-var answersEl=document.getElementById("answers");
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+];
+var time = 60;
+var TimerEl = document.getElementById("Timer");
+var startEl = document.getElementById("start");
+var startbtnEl = document.getElementById("start-btn");
+var quizEl = document.getElementById("Quiz-play");
+var questionEl = document.getElementById("question");
+var answersEl = document.getElementById("answers");
 var timeinterval;
-var index=0
+var index = 0
 
 function startQuiz() {
-    startEl.classList.add("hide")
-    quizEl.classList.remove("hide")
-    timeinterval=setInterval(countdown, 1000)
-    TimerEl.textContent=time
-    displayQuestions()
+  startEl.classList.add("hide")
+  quizEl.classList.remove("hide")
+  timeinterval = setInterval(countdown, 1000)
+  TimerEl.textContent = time
+  displayQuestions()
 }
 function countdown() {
-    time--
-    TimerEl.textContent=time
-    if(time<=0){
-        stop();        
-    }
+  time--
+  TimerEl.textContent = time
+  if (time <= 0) {
+    stop();
+  }
 }
 function displayQuestions() {
-    var currentQuestion=questions [index]
-    questionEl.textContent=currentQuestion.title
-    answersEl.innerHTML=""
-    currentQuestion.choices.forEach(function(choice){
-        var btn=document.createElement("button");
-        btn.textContent=choice
-        btn.onclick=checkanswer;
-        answersEl.append(btn);
-    })
+  var currentQuestion = questions[index]
+  questionEl.textContent = currentQuestion.title
+  answersEl.innerHTML = ""
+  currentQuestion.choices.forEach(function (choice) {
+    var btn = document.createElement("button");
+    btn.textContent = choice
+    btn.onclick = checkanswer;
+    answersEl.append(btn);
+  })
 }
 function checkanswer(event) {
-    
+  if (event.target.textContent !== questions[index].answer) {
+    time -= 5
+    TimerEl.textContent = time
+    if (time <= 0) {
+      time = 0
+      stop();
+    }
+  } else { }
+  index++
+  if (index === questions.length) {
+    stop();
+  }
+  else { displayQuestions() }
 }
-startbtnEl.onclick=startQuiz
+function stop() {
+  clearInterval(timeinterval)
+  quizEl.classList.add("hide")
+  document.querySelector('.end-quiz').classList.remove("hide")
+  document.getElementById('score').textContent=time
+  document.getElementById('save').onclick=saveScore
+}
+function saveScore() {
+  const initials=document.getElementById('initials').value
+  if (!initials) return
+  const newScore={
+    initials,score:time
+  }
+  const scores=JSON.parse(localStorage.getItem('scores'))||[]
+  scores.push(newScore)
+  localStorage.setItem('scores',JSON.stringify(scores))
+  
+}
+startbtnEl.onclick = startQuiz
